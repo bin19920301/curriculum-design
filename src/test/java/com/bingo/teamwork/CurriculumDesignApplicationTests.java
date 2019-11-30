3cc56af96be1c755feb60a0a59c06e44ed08c1b1
赵如滨 <24455913@qq.com>
2019-11-30 13:15:03 +0800

    init

---
+++ src/test/java/com/bingo/teamwork/CurriculumDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bingo.teamwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculumDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
