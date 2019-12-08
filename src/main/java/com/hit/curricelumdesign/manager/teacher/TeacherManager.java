016e68fc3d50e030dad9db087807abc8ab957783
赵如滨 <24455913@qq.com>
2019-12-08 22:43:56 +0800

    teacherManage teacherService update

--- src/main/java/com/hit/curricelumdesign/manager/teacher/TeacherManager.java
+++ src/main/java/com/hit/curricelumdesign/manager/teacher/TeacherManager.java
@@ -1,15 +1,12 @@
 package com.hit.curricelumdesign.manager.teacher;
 
 import com.hit.curricelumdesign.context.dto.teacher.TeacherDTO;
-import com.hit.curricelumdesign.context.entity.Teacher;
 import com.hit.curricelumdesign.context.enums.Error;
 import com.hit.curricelumdesign.context.exception.BaseException;
 import com.hit.curricelumdesign.dao.TeacherMapper;
-import com.hit.curricelumdesign.utils.BeanUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Date;
 import java.util.List;
 
 @Component
