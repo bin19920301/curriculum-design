76e8eb348487d55bc15e2a3908ab92f3d2a2ca3b
赵如滨 <24455913@qq.com>
2019-12-21 11:50:53 +0800

    part of work update

--- src/main/java/com/hit/curricelumdesign/context/param/work/UpdateWorkParam.java
+++ src/main/java/com/hit/curricelumdesign/context/param/work/UpdateWorkParam.java
@@ -2,4 +2,18 @@ package com.hit.curricelumdesign.context.param.work;
 
 public class UpdateWorkParam extends WorkBaseParam {
 
+    /**
+     * 作业项目id
+     */
+    private Integer workProjectId;
+
+
+    public Integer getWorkProjectId() {
+        return workProjectId;
+    }
+
+    public void setWorkProjectId(Integer workProjectId) {
+        this.workProjectId = workProjectId;
+    }
+
 }
--- src/main/java/com/hit/curricelumdesign/service/WorkService.java
+++ src/main/java/com/hit/curricelumdesign/service/WorkService.java
@@ -1,11 +1,17 @@
 package com.hit.curricelumdesign.service;
 
+import com.hit.curricelumdesign.context.constant.Constants;
+import com.hit.curricelumdesign.context.entity.Work;
+import com.hit.curricelumdesign.context.param.work.AddWorkParam;
 import com.hit.curricelumdesign.context.response.Result;
 import com.hit.curricelumdesign.dao.WorkMapper;
 import com.hit.curricelumdesign.manager.work.WorkManager;
+import com.hit.curricelumdesign.utils.BeanUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 @Service
 @Transactional
@@ -18,7 +24,20 @@ public class WorkService {
     private WorkManager workManager;
 
 
-    public Result addWork() {
+    public Result addWork(AddWorkParam workParam) {
+        Work work = new Work();
+        BeanUtil.copyProperties(workParam,work);
+        //设置分数
+        work.setScore(0);
+        //设置状态
+        work.setStatus(0);
+        //设置未删除
+        work.setIsDelete(Constants.Common.IS_NOT);
+        //设置创建时间
+        work.setCreatetime(new Date());
+        //设置修改时间
+        work.setUpdatetime(new Date());
+        workMapper.insert(work);
         return Result.success();
     }
 
