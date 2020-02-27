2404be3a53fbc720dade8987e986c1229b407730
xubaorui123 <xubaorui123@163.com>
2020-02-27 16:14:12 +0800

    作业详情

--- src/main/java/com/hit/curricelumdesign/context/dto/work/WorkInfoDTO.java
+++ src/main/java/com/hit/curricelumdesign/context/dto/work/WorkInfoDTO.java
@@ -30,11 +30,12 @@ public class WorkInfoDTO {
 
     //private List<CraftCardInfoDTO> craftCardInfoDTOList;
 
-    //private List<WorkFileDTO> workFileDTOList;
+    private List<WorkFileDTO> workFileDTOList;
 
     //2020-02-23 注释掉上面两行LIST属性,新的属性包含在cardDTO中
     //2020-02-23 新增card接口,这里不再需要card信息
     //private CardDTO cardDTO;
+
 
     public Integer getWorkId() {
         return workId;
@@ -76,4 +77,23 @@ public class WorkInfoDTO {
         this.workProjectInfoDTO = workProjectInfoDTO;
     }
 
+    public List<WorkFileDTO> getWorkFileDTOList() {
+        return workFileDTOList;
+    }
+
+    public void setWorkFileDTOList(List<WorkFileDTO> workFileDTOList) {
+        this.workFileDTOList = workFileDTOList;
+    }
+
+    @Override
+    public String toString() {
+        return "WorkInfoDTO{" +
+                "workId=" + workId +
+                ", status=" + status +
+                ", studentName='" + studentName + '\'' +
+                ", teaching=" + teaching +
+                ", workProjectInfoDTO=" + workProjectInfoDTO +
+                ", workFileDTOList=" + workFileDTOList +
+                '}';
+    }
 }
