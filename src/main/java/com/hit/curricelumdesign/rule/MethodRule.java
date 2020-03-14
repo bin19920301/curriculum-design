5751d4463fea46168a2dd2e61905d5ae203e8a30
赵如滨 <24455913@qq.com>
2020-03-14 22:52:55 +0800

    加工规则实现

--- src/main/java/com/hit/curricelumdesign/rule/MethodRule.java
+++ src/main/java/com/hit/curricelumdesign/rule/MethodRule.java
@@ -1,6 +1,12 @@
 package com.hit.curricelumdesign.rule;
 
+import com.hit.curricelumdesign.context.bo.rule.FinishedMethodBO;
+import com.hit.curricelumdesign.context.bo.rule.RuleBO;
 import com.hit.curricelumdesign.context.bo.rule.WorkCardBO;
+import com.hit.curricelumdesign.utils.StringUtil;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * @author xbr
@@ -9,8 +15,71 @@ import com.hit.curricelumdesign.context.bo.rule.WorkCardBO;
  * @Description
  */
 public class MethodRule implements Rule {
+
+    //表面id
+    private Integer surfaceId;
+
+    //加工方法
+    //加工方法ID：车削-a；铣削-b；刨-c；磨-d；钻-e；铰-f
+    //设计出现错误 数据库设计时: 机床;1车床,2铣床,3刨床,4磨床
+    private String methodCharacter;
+
+    //RuleBO
+    private RuleBO ruleBO;
+
+    public MethodRule(Integer surfaceId, String methodCharacter, RuleBO ruleBO) {
+        this.surfaceId = surfaceId;
+        this.methodCharacter = methodCharacter;
+        this.ruleBO = ruleBO;
+    }
+
     @Override
     public void check(WorkCardBO workCardBO) {
+        ArrayList<FinishedMethodBO> finishedMethodBOList = workCardBO.getFinishedMethodBOList();
+        //list的非空判断
+        if (finishedMethodBOList.isEmpty()){
+            return;
+        }
+        //使用迭代器,可以删除已经判断过的规则
+        Iterator<FinishedMethodBO> iterator = finishedMethodBOList.iterator();
+        while(iterator.hasNext()){
+            FinishedMethodBO finishedMethodBO = iterator.next();
+            //找到相同的加工表面id
+            if (finishedMethodBO.getSurfaceId() == this.surfaceId){
+                //判断加工方法是否正确
+                String method = finishedMethodBO.getMethod();
+                if (StringUtil.isBlank(method) || !method.equals(methodCharacter)){
+                    finishedMethodBO.addErrorMsg(ruleBO.getRuleId(),ruleBO.getRuleString());
+                }
+                //该工位的加工规则已经判断过了,从集合中移除
+                iterator.remove();
+            }
+        }
 
+
+    }
+
+    public Integer getSurfaceId() {
+        return surfaceId;
+    }
+
+    public void setSurfaceId(Integer surfaceId) {
+        this.surfaceId = surfaceId;
+    }
+
+    public String getMethodCharacter() {
+        return methodCharacter;
+    }
+
+    public void setMethodCharacter(String methodCharacter) {
+        this.methodCharacter = methodCharacter;
+    }
+
+    public RuleBO getRuleBO() {
+        return ruleBO;
+    }
+
+    public void setRuleBO(RuleBO ruleBO) {
+        this.ruleBO = ruleBO;
     }
 }
--- src/main/java/com/hit/curricelumdesign/rule/MethodRuleFactory.java
+++ src/main/java/com/hit/curricelumdesign/rule/MethodRuleFactory.java
@@ -1,8 +1,10 @@
 package com.hit.curricelumdesign.rule;
 
 import com.hit.curricelumdesign.context.bo.rule.RuleBO;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,6 +17,22 @@ import java.util.List;
 public class MethodRuleFactory implements RuleFactory {
     @Override
     public List<Rule> resolve(RuleBO ruleBO) {
-        return null;
+        String ruleString = ruleBO.getRuleString();
+        List<Rule> ruleList = new ArrayList<>();
+
+        Rule rule;
+        if (!StringUtils.isBlank(ruleString)){
+            //分解出每一组<表面id,加工方法>
+            String[] ruleStringArray = ruleString.split(";");
+            for (String  untreatedRuleString: ruleStringArray) {
+                String[] ruleArray = untreatedRuleString.split(",");
+                //组装成加工方法得规则
+                MethodRule methodRule = new MethodRule(Integer.valueOf(ruleArray[0]), ruleArray[1], ruleBO);
+                //组装规则list
+                ruleList.add(methodRule);
+            }
+        }
+        return ruleList;
     }
+
 }
