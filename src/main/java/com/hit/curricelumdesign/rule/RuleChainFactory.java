444e6456b364b6f79e796bb3574f1a40a1593173
xubaorui123 <xubaorui123@163.com>
2020-03-14 18:02:50 +0800

    规则校验

--- src/main/java/com/hit/curricelumdesign/rule/RuleChainFactory.java
+++ src/main/java/com/hit/curricelumdesign/rule/RuleChainFactory.java
@@ -1,10 +1,8 @@
 package com.hit.curricelumdesign.rule;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 
